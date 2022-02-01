0a7126554e07b57a3549a7b775e6f3d37ea7d37d
Marvin Edem Kudjo <33563441+Marvinkwame@users.noreply.github.com>
2022-02-01 14:30:25 +0000

    Update Info.js

--- src/components/Info.js
+++ src/components/Info.js
@@ -16,7 +16,7 @@ export default function Info() {
             <div className="info-btns">
                 <div>
                     <button className="email-btn" type="button">
-                        <img src={email} className="email-logo" alt="email-logo" /><a href="mailto: marvinkudjo@gmail.com"></a>
+                        <a href="mailto: marvinkudjo@gmail.com"><img src={email} className="email-logo" alt="email-logo" /></a>
                         Email</button>
                 </div>
 
